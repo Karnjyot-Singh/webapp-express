bcac7958d0c472930267f0920b15b697a7eba7ef
Karnjyot <krnjyotsingh2004@email.com>
2025-01-30 03:28:53 +0100

    feat: cora

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const moviesRouter = require("./routers/movies");
 const errorsHandler = require("./middleware/errorsHandler");
+const cors = require("cors");
 
 //creazione dell'app express 
 const app = express();
 const port = process.env.SERVER_PORT;
+
+//Middleware delle CORS
+app.use(cors({
+    origin: process.env.FRONEND_URL
+}))
 
 //Middleware per rendere la cartella pubblica accessibile da fuori
 app.use(express.static(`public`));
